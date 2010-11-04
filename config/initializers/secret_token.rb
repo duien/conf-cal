0536f251ca0d08697b67579c68d1df35c2be9974
Emily Price <price.emily@gmail.com>
2010-11-04 16:30:19 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConfCal::Application.config.secret_token = '29680e8c7bd4f13c894897b09f1cf17b8c9b36b454a9a58aa51ba5be808bb12e102f9c2e4c9447a9556186d6aa42da693c8bd809e71023e5f78276c4e156b71d'
