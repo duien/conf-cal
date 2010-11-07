ebf0b88fa6e4ead37c78c217f6ac75cc9fa2a9b5
Emily Price <price.emily@gmail.com>
2010-11-07 14:33:24 -0500

    Change the way confererence_sessions#index works, tweak some views

--- app/controllers/conference_sessions_controller.rb
+++ app/controllers/conference_sessions_controller.rb
@@ -2,17 +2,20 @@ class ConferenceSessionsController < ApplicationController
   before_filter :login_required, :only => :index
 
   def index
-    sessions= if params[:date] and params[:time]
+    if params[:date] and params[:time]
       time = DateTime.parse("#{params[:date]} #{params[:time]}")
-      ConferenceSession.all( #:conditions => { :start_time => time } )
+      @conference_sessions = ConferenceSession.all(
         :conditions => ["datetime(start_time) = datetime(?)", time],
-        :include => :attendees )
+        :include => :attendees,
+        :order => :start_time)
+      render 'date_time_index'
     else
-      ConferenceSession.all(:include => :attendees)
-    end
-    sessions = sessions.group_by { |p| p.start_time.strftime('%A') }
-    @conference_sessions = sessions.inject({}) do |result, (day, session_list)|
-      result.update(day => session_list.group_by(&:start_time))
+      sessions = ConferenceSession.all(:include => :attendees, :order => :start_time)
+      sessions = sessions.group_by { |p| p.start_time.strftime('%A') }
+      @conference_sessions = sessions.inject({}) do |result, (day, session_list)|
+        result.update(day => session_list.group_by(&:start_time))
+      end
+      render
     end
   end
 
