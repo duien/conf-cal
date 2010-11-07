95cc46dfb6c453d3cd7e6613183ce648f7120f78
John Hyland <jhyland@damballa.com>
2010-11-07 13:42:45 -0500

    tinkering with friends stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,28 @@ class User < TwitterAuth::GenericUser
 
   FRIEND_CACHE_DURATION = 10.minutes
 
-  # Update cached friends if the cache has timed out
-  def after_find
-    if last_updated_friends.nil? or (Time.now - last_updated_friends > FRIEND_CACHE_DURATION)
-      last_updated_friends = Time.now
+  # Override friends to check the cache and update from Twitter as necessary.
+  #
+  alias :orig_friends :friends
+  def friends
+    if self.last_updated_friends.nil? or (Time.now - self.last_updated_friends > FRIEND_CACHE_DURATION)
+      self.update_friends
+      self.last_updated_friends = Time.now
       self.save!
-      begin
-        friends = User.find( :conditions => {:twitter_id => self.twitter.get('/friends/ids.json')} )
-      rescue Exception => e
-        logger.error "Could not fetch friends for user #{self.id} from twitter: #{e}"
-      end
     end
+    orig_friends
   end
 
+  # Fetch this user's friends from Twitter and set up Friendships as
+  # appropriate.  Returns true if succesful, false otherwise.
+  #
+  def update_friends
+    begin
+      self.friends = User.find( :all, :conditions => {:twitter_id => self.twitter.get('/friends/ids.json')} )
+      return true
+    rescue Exception => e
+      logger.error "Could not fetch friends for user #{self.id} from twitter: #{e}"
+      return false
+    end
+  end
 end
