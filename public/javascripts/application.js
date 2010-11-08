5fe8b5601139408a2f4d37be91737f67b29f9b04
Emily Price <price.emily@gmail.com>
2010-11-07 23:58:03 -0500

    Make I'm Going links work on time slot view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function mark_attending(uid, csid, time) {
+function mark_attending(uid, csid, time, url) {
   $.post( "/attendances", { "attendance[user_id]": uid, "attendance[conference_session_id]": csid }, function () { 
-    $.get("/conference_sessions/for/" + time, function (data) {
-      $("#timeslot_" + time).replaceWith( data );
-    });
+    if(url){
+      window.location.replace(url);
+    }else{
+      $.get("/conference_sessions/for/" + time, function (data) {
+        $("#timeslot_" + time).replaceWith( data );
+      });
+    }
   });
 }
