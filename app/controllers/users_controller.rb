b7346d0c9aead29e5cf23407a7e1a3efbfce0123
John Hyland <jhyland@damballa.com>
2010-11-08 00:43:17 -0500

    rogue ical code snuck in.  :-/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,22 +6,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.ics do
-
-        @cal = Icalendar::Calendar.new
-        @user.conference_sessions.each do |cs|
-          @cal.event do
-            dtstart     cs.start_time
-            dtend       cs.end_time
-            summary     cs.summary
-            description cs.description
-            location    cs.location
-          end
-        end
-        @cal.publish
-
-        render 'show.ics.haml', :layout => false 
-      end
+      format.ics { render 'show.ics.haml', :layout => false }
     end
   end
 end
