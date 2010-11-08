683aa9ee28ce7398e1121c0b6d695e3bf7515c21
Emily Price <price.emily@gmail.com>
2010-11-08 00:46:22 -0500

    Make friends#index a little prettier

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,7 +2,7 @@ class FriendsController < ApplicationController
   before_filter :login_required
 
   def index
-    @friends = current_user.friends
+    @friends = current_user.friends.all( :include => :conference_sessions )
 
     respond_to do |format|
       format.html # index.html.erb
