2768169406e49e9ce25d925ac97369a9a2f142bc
John Hyland <jhyland@damballa.com>
2010-11-08 00:39:59 -0500

    add ical support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
+  include Icalendar
+
   def show
     @user = User.find params[:id]
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
+      format.ics do
+
+        @cal = Icalendar::Calendar.new
+        @user.conference_sessions.each do |cs|
+          @cal.event do
+            dtstart     cs.start_time
+            dtend       cs.end_time
+            summary     cs.summary
+            description cs.description
+            location    cs.location
+          end
+        end
+        @cal.publish
+
+        render 'show.ics.haml', :layout => false 
+      end
     end
   end
 end
