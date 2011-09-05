ccd8db33a1463c9259f4062f73fee062f9019ae3
Emily Price <price.emily@gmail.com>
2011-09-05 01:30:21 -0400

    Use current conference in next/prev unscheduled queries

--- app/models/conference_session.rb
+++ app/models/conference_session.rb
@@ -12,13 +12,13 @@ class ConferenceSession < ActiveRecord::Base
   scope :earlier_than,    lambda { |time| where( 'start_time < ?', time ) }
 
   def self.next_unscheduled_timeslot (user, time)
-    sql = "SELECT min(start_time) FROM conference_sessions cs WHERE start_time > '#{time.to_s(:db)}' AND NOT EXISTS (SELECT 'x' FROM attendances a, conference_sessions cs2 WHERE a.user_id = #{user.id} AND a.conference_session_id = cs2.id AND cs.start_time = cs2.start_time)" 
+    sql = "SELECT min(start_time) FROM conference_sessions cs WHERE start_time > '#{time.to_s(:db)}' AND cs.conference_id = #{Conference::CURRENT} AND NOT EXISTS (SELECT 'x' FROM attendances a, conference_sessions cs2 WHERE a.user_id = #{user.id} AND a.conference_session_id = cs2.id AND cs.start_time = cs2.start_time)" 
     string = ActiveRecord::Base.connection.select_rows(sql).first.first
     string.nil? ? nil : Time.parse(string)
   end
 
   def self.prev_unscheduled_timeslot (user, time)
-    sql = "SELECT max(start_time) FROM conference_sessions cs WHERE start_time < '#{time.to_s(:db)}' AND NOT EXISTS (SELECT 'x' FROM attendances a, conference_sessions cs2 WHERE a.user_id = #{user.id} AND a.conference_session_id = cs2.id AND cs.start_time = cs2.start_time)" 
+    sql = "SELECT max(start_time) FROM conference_sessions cs WHERE start_time < '#{time.to_s(:db)}' AND cs.conference_id = #{Conference::CURRENT} AND NOT EXISTS (SELECT 'x' FROM attendances a, conference_sessions cs2 WHERE a.user_id = #{user.id} AND a.conference_session_id = cs2.id AND cs.start_time = cs2.start_time)" 
     string = ActiveRecord::Base.connection.select_rows(sql).first.first
     string.nil? ? nil : Time.parse(string)
   end
